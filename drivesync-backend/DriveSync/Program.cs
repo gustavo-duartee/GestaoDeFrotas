6751dd9476d8778928e30d4ec8e90e9808c9bd01
Daniel Peralba <contatodanielperalba@hotmail.com>
2024-04-22 18:15:38 -0300

    Implementando envio de chave no swagger

--- drivesync-backend/DriveSync/Program.cs
+++ drivesync-backend/DriveSync/Program.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -54,6 +56,39 @@ builder.Services.AddScoped<IAuthenticate, AuthenticateService>();
 
 builder.Services.AddScoped<VeiculosService>();
 builder.Services.AddCors();
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DriveSync.API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme." +
+        " \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below." +
+        "\r\n\r\n Example: \"Bearer 12345abcdef\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
 
 var app = builder.Build();
 
