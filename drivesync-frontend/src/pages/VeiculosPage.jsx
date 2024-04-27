1fb1f90a446f483ee680b73f7c2bd98597632fc6
Daniel Campos <contatodanielperalba@hotmail.com>
2024-04-27 15:27:52 -0300

    Arrumando filtro

--- drivesync-frontend/src/pages/VeiculosPage.jsx
+++ drivesync-frontend/src/pages/VeiculosPage.jsx
@@ -126,7 +126,7 @@ export function Veiculos() {
               />
 
               <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white" style={{ maxHeight: "40rem", overflow: "auto" }}>
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500" style={{ height: "40rem" }}>
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500">
 
                   {/* Cabeçalho da tabela */}
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 border">
@@ -192,82 +192,77 @@ export function Veiculos() {
 
                   {/* Linha da tabela */}
                   <tbody>
-                    {searchInput.length > 1 && filtro.map(veiculo => (
-                      <tr key={veiculo.id} className="bg-white border-b">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                          {veiculo.placa}
-                        </th>
-                        <td className="px-6 py-4">
-                          {veiculo.marca}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.modelo}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.ano}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.quilometragem}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.dt_aquisicao}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.tp_combustivel}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.status}
-                        </td>
-                        <td className="px-6 py-4 text-right">
-                          <button type="button" onClick={() => excluirVeiculo(veiculo.id)} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Excluir</button>
-                        </td>
-                        <td className="px-6 py-4 text-right">
-                          <button type="button" onClick={() => editVeiculo(veiculo.id)} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Editar</button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-
-
-                  <tbody>
-                    {veiculos.map(veiculo => (
-                      <tr key={veiculo.id} className="bg-white border-b">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                          {veiculo.placa}
-                        </th>
-                        <td className="px-6 py-4">
-                          {veiculo.marca}
-
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.modelo}
-
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.ano}
-
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.quilometragem}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.dt_aquisicao}
-
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.tp_combustivel}
-                        </td>
-                        <td className="px-6 py-4">
-                          {veiculo.status}
-                        </td>
-                        <td className="px-6 py-4 text-right">
-                          <button type="button" onClick={() => excluirVeiculo(veiculo.id)} class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Excluir</button>
-                        </td>
-                        <td className="px-6 py-4 text-right">
-                          <button type="button" onClick={() => editVeiculo(veiculo.id)} class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Editar</button>
-                        </td>
-                      </tr>
-                    ))}
+                    {searchInput.length > 1 ? (
+                        filtro.map(veiculo => (
+                          <tr key={veiculo.id} className="bg-white border-b">
+                            <td className="px-6 py-4">
+                              {veiculo.placa}
+                            </td>
+                            <td className="px-6 py-4">
+                              {veiculo.marca}
+                            </td>
+                            <td className="px-6 py-4">
+                              {veiculo.modelo}
+                            </td>
+                            <td className="px-6 py-4">
+                              {veiculo.ano}
+                            </td>
+                            <td className="px-6 py-4">
+                              {veiculo.quilometragem}
+                            </td>
+                            <td className="px-6 py-4">
+                              {veiculo.dt_aquisicao}
+                            </td>
+                            <td className="px-6 py-4">
+                              {veiculo.tp_combustivel}
+                            </td>
+                            <td className="px-6 py-4">
+                              {veiculo.status}
+                            </td>
+                            <td className="px-6 py-4 text-right">
+                              <button type="button" onClick={() => excluirVeiculo(veiculo.id)} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Excluir</button>
+                            </td>
+                            <td className="px-6 py-4 text-right">
+                              <button type="button" onClick={() => editVeiculo(veiculo.id)} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Editar</button>
+                            </td>
+                          </tr>
+                          ))
+                    ) : (
+                      veiculos.map(veiculo => (
+                    <tr key={veiculo.id} className="bg-white border-b">
+                      <td className="px-6 py-4">
+                        {veiculo.placa}
+                      </td>
+                      <td className="px-6 py-4">
+                        {veiculo.marca}
+                      </td>
+                      <td className="px-6 py-4">
+                        {veiculo.modelo}
+                      </td>
+                      <td className="px-6 py-4">
+                        {veiculo.ano}
+                      </td>
+                      <td className="px-6 py-4">
+                        {veiculo.quilometragem}
+                      </td>
+                      <td className="px-6 py-4">
+                        {veiculo.dt_aquisicao}
+                      </td>
+                      <td className="px-6 py-4">
+                        {veiculo.tp_combustivel}
+                      </td>
+                      <td className="px-6 py-4">
+                        {veiculo.status}
+                      </td>
+                      <td className="px-6 py-4 text-right">
+                        <button type="button" onClick={() => excluirVeiculo(veiculo.id)} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Excluir</button>
+                      </td>
+                      <td className="px-6 py-4 text-right">
+                        <button type="button" onClick={() => editVeiculo(veiculo.id)} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Editar</button>
+                      </td>
+                    </tr>
+                    ))
+                    )}
                   </tbody>
                 </table>
               </div>
