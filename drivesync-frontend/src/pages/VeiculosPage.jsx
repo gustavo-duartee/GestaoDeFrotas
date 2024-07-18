04e7efd4b210f3c5078ffa9e4c79e498cfa03344
mageuss <114534224+mageuss@users.noreply.github.com>
2024-07-18 16:39:33 -0300

    criei uma tabela colapsavel, no veiculo daria pra ver as manutencoes

--- drivesync-frontend/src/pages/VeiculosPage.jsx
+++ drivesync-frontend/src/pages/VeiculosPage.jsx
@@ -1,8 +1,25 @@
+//#region imports
 import React, { useState, useEffect } from "react";
 import { Sidebar } from "../components/Sidebar";
 import { ModalCriarVeiculo } from '../components/Veiculos/ModalCriarVeiculos';
 import { ModalEditarVeiculo } from '../components/Veiculos/ModalEditarVeiculos';
 import api from '../services/api';
+import * as React from 'react';
+import { Box, Collapse, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Paper, TablePagination } from '@mui/material';
+// import Collapse from '@mui/material/Collapse';
+// import IconButton from '@mui/material/IconButton';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+// import Typography from '@mui/material/Typography';
+// import Paper from '@mui/material/Paper';
+import { KeyboardArrowDownIcon, KeyboardArrowUpIcon } from '@mui/icons-material';
+// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+//#endregion
 
 export function Veiculos() {
   const [searchInput, setSearchInput] = useState('');
@@ -11,6 +28,8 @@ export function Veiculos() {
   const [modalCreateIsOpen, setModalCreateIsOpen] = useState(false);
   const [modalEditIsOpen, setModalEditIsOpen] = useState(false);
   const [veiculoId, setVeiculoId] = useState(null);
+  const [manutencao, getManutencao] = useState(''),
+  const [manutencaoId, getManutencaoId] = useState(''),
 
   const token = localStorage.getItem('token');
   const authorization = {
@@ -18,6 +37,8 @@ export function Veiculos() {
       Authorization: `Bearer ${token}`
     }
   }
+
+//#region chamadas de api
 
   useEffect(() => {
     api.get('api/veiculos', authorization)
@@ -28,6 +49,17 @@ export function Veiculos() {
         console.error('Erro ao obter veículo: ', error);
       });
   }, []);
+
+  useEffect(() => {
+    api.get('api/mantencoes', authorization)
+        .then(response => {
+            getManutencao(response.data);
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('Erro ao obter manutenção: ', error);
+        });
+}, []);
 
   const searchVeiculos = (searchValue) => {
     setSearchInput(searchValue);
@@ -42,22 +74,6 @@ export function Veiculos() {
       setFiltro(veiculos);
     }
   }
-
-  const openEditModal = (id) => {
-    setVeiculoId(id);
-    setModalEditIsOpen(true); // Abre o modal de edição
-  }
-
-  const openCreateModal = () => {
-    setModalCreateIsOpen(true); // Abre o modal de criação
-  }
-
-  const closeModal = () => {
-    setModalCreateIsOpen(false); // Fecha o modal de criação
-    setModalEditIsOpen(false); // Fecha o modal de edição
-    setVeiculoId(null);
-  }
-
   const editVeiculo = async (id) => {
     try {
       console.log(`Editar veículo com ID: ${id}`);
@@ -93,6 +109,187 @@ export function Veiculos() {
       alert(errorMessage);
     }
   }
+  //#endregion
+
+//#region chamada de modais
+  const openEditModal = (id) => {
+    setVeiculoId(id);
+    setModalEditIsOpen(true); // Abre o modal de edição
+  }
+
+  const openCreateModal = () => {
+    setModalCreateIsOpen(true); // Abre o modal de criação
+  }
+
+  const closeModal = () => {
+    setModalCreateIsOpen(false); // Fecha o modal de criação
+    setModalEditIsOpen(false); // Fecha o modal de edição
+    setVeiculoId(null);
+  }
+
+  //#endregion
+
+  function createData(
+    marca,
+    modelo,
+    ano,
+    placa,
+    quilometragem,
+    tp_combustivel,
+    dt_aquisicao,
+    status,
+  ) {
+    return {
+      marca,
+      modelo,
+      ano,
+      placa,
+      quilometragem,
+      tp_combustivel,
+      dt_aquisicao,
+      status,
+      manutencoes: [
+        {
+          dt_manutencao,
+          dt_proxmanutencao,
+          tp_manutencao,
+          servico,
+          valor,
+          descricao,
+        },
+      ],
+    };
+  }
+
+
+  function Row(props) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
+
+    return (
+      <React.Fragment>
+        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+          <TableCell>
+            <IconButton
+              aria-label="expand row"
+              size="small"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+          <TableCell component="th" scope="row">
+            {veiculo.id}
+          </TableCell>
+          <TableCell align="right">{veiculo.placa}</TableCell>
+          <TableCell align="right">{veiculo.marca}</TableCell>
+          <TableCell align="right">{veiculo.modelo}</TableCell>
+          <TableCell align="right">{veiculo.ano}</TableCell>
+          <TableCell align="right">{veiculo.quilometragem}</TableCell>
+          <TableCell align="right">{veiculo.dt_aquisicao}</TableCell>
+          <TableCell align="right">{veiculo.tp_combustivel}</TableCell>
+          <TableCell align="right">{veiculo.status}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box sx={{ margin: 1 }}>
+                <Typography variant="h6" gutterBottom component="div">
+                  Manutenção
+                </Typography>
+                <Table size="small" aria-label="purchases">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Data da Última Manutenção</TableCell>
+                      <TableCell>Próxima Manutenção</TableCell>
+                      <TableCell>Tipo da Manutenção</TableCell>
+                      <TableCell>Veículo</TableCell>
+                      <TableCell>Serviço</TableCell>
+                      <TableCell>Valor</TableCell>
+                      <TableCell>Descrição</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {row.manutencoes.map((manutencao) => (
+                      <TableRow key={manutencao.id}>
+                        <TableCell component="th" scope="row">{historyRow.date}</TableCell>
+                        <TableCell align="right">{manutencao.dt_manutencao}</TableCell>
+                        <TableCell align="right">{manutencao.dt_prox_manutencao}</TableCell>
+                        <TableCell align="right">{manutencao.tp_manutencao}</TableCell>
+                        <TableCell align="right">{manutencao.veiculo}</TableCell>
+                        <TableCell align="right">{manutencao.servico}</TableCell>
+                        <TableCell align="right">{manutencao.valor}</TableCell>
+                        <TableCell align="right">{manutencao.descricao}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
+
+  // const rows = [
+  //   createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+  //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
+  //   createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
+  //   createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
+  //   createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+  // ];
+
+  export default function CollapsibleTable() {
+    return (
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              {/* <TableCell>Dessert (100g serving)</TableCell> */}
+              <TableCell align="right">Marca</TableCell>
+              <TableCell align="right">Modelo</TableCell>
+              <TableCell align="right">Ano</TableCell>
+              <TableCell align="right">Placa</TableCell>
+              <TableCell align="right">Quilometragem</TableCell>
+              <TableCell align="right">Tipo&nbsp;de Combustível</TableCell>
+              <TableCell align="right">Data&nbsp;Aquisição</TableCell>
+              <TableCell align="right">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {veiculos.map((veiculo) => (
+              <Row key={veiculo.id} row={veiculo} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <div>
@@ -177,7 +374,6 @@ export function Veiculos() {
                   </tbody>
                 </table>
               </div>
-              
             </div>
           </main>
         </div>
