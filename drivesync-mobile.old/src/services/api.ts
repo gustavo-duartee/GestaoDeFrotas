96fe939b13929c4996a94f9412bca60fa1c8da25
mageuss <mateus.santos@aedb.br>
2024-11-29 03:19:00 -0900

    apaguei o mobile antigo

--- drivesync-mobile.old/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import API_BASE_URL from "../config/config"; // Importe a URL do arquivo de configuração
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-export default api;
--- drivesync-mobile.old/src/types.tsx
+++
@@ -1,5 +0,0 @@
-// types.ts
-export type RootStackParamList = {
-  Login: undefined;
-  Home: undefined;
-};
